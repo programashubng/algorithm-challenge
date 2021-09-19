f8b902628d62cd67efe8640d5715f5c9fe0cb04e
Cradoe <me@ismailobadimu.com>
2021-09-19 02:39:27 +0100

    Racheal Adeyemi's submission

--- bin/max_value.dart
+++
@@ -1,8 +0,0 @@
-
-import 'package:max_value/max_value.dart';
-
-void main(List<String> arguments) {
-    var myarr=[22,53,1,9,2,80];
-  var largest = maxValue(myarr);
-  print('The largest number in the array is $largest');
-}
--- test/max_value_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:max_value/max_value.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
